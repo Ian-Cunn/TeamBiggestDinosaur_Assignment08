b2413ce1ae413a8cf4bbcda0df55629bc021a93f
ensta <ensta@LAPTOP-56R9U5BD.lan>
2024-03-27 22:13:57 -0400

    Last Commit

--- TeamBiggestDinosaur_Assignment08/mainPackage/main.py
+++ TeamBiggestDinosaur_Assignment08/mainPackage/main.py
@@ -1,11 +1,11 @@
 # main.py
-# Name: TeamBiggestDinosaur (Harsh Shah, Ian Cunninghan, and Elizabeth Stapelton)
-# email: shahh4@mail.uc.edu
+# Name: TeamBiggestDinosaur (Harsh Shah, Ian Cunningham, and Elizabeth Stapleton)
+# email: shahh4@mail.uc.edu, stapleet@mail.uc.edu, cunninig@mail.uc.edu
 # Assignment Number: Assignment 08
 # Due Date: 03/28/2024
 # Course/Section: IS4010-002
 # Semester/Year: Spring 2024
-# Brief Description of the assignment: It prints the data from grocery store stimulator database online. 
+# Brief Description of the assignment: It prints the data from grocery store simulator database online. 
 # Anything else that's relevant: Used worked done in class as a reference. 
 
 from dataClassPackage.dataClass import Data
